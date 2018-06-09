793f3a40bb4fd44e9b2a0f0ad2af86942ce1f793
Andreas Motl <andreas.motl@elmyra.de>
2018-06-09 02:46:34 +0200

    jQuery3 compatibility re. "remoteAttr" method

--- js/jquery.jqpagination.js
+++ js/jquery.jqpagination.js
@@ -66,7 +66,7 @@
 			}
 
 			// remove the readonly attribute as JavaScript must be working by now ;-)
-			base.$input.removeAttr('readonly');
+			base.$input.prop('readonly', false);
 
 			// set the initial input value
 			// pass true to prevent paged callback form being fired
